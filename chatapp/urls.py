5fb731e619f5dd1069eba9e42e22ba21fcd603e0
Princy Angeline J <princyangeline.j@techjays.com>
2026-01-14 23:48:13 +0530

    first commit

--- chatapp/urls.py
+++ chatapp/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for chatgpt_clone project.
+URL configuration for chatapp project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
